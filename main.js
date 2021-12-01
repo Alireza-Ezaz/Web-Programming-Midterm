34717d89a9abb095414d6e03f144883a97e36b3c
S.Alireza Ezaz <sar.ezaz@yahoo.com>
2021-12-01 13:46:01 +0330

    Added api call by user input and store response in javascript object.

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const findGender = async () => {
     let name = document.getElementById('name').value;
     console.log(name);
     const response = await fetch('https://api.genderize.io/?name=' + name);
-    const responseJson = await response.json(); //extract JSON from the http response
-    console.log(responseJson);
+    let person = {name: '', gender: '', probability: 0, count: 0};
+    await response.json().then(result => {  //extract JSON from the http response and store in javascript object
+        person.count = result['count'];
+        person.name = result['name'];
+        person.gender = result['gender'];
+        person.probability = result['probability'];
+    });
+
+    document.getElementById("prediction-probability").innerHTML = person.probability; // show the probability in html
+    document.getElementById("prediction-gender").innerHTML = person.gender; // show the gender in html
 }
