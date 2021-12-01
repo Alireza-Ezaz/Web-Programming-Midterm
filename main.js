ed3cb19cae33b1a524583855d34951e3debdd52b
S.Alireza Ezaz <sar.ezaz@yahoo.com>
2021-12-01 13:34:18 +0330

    Added prediction functionality.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const findGender = async () => {
-    const response = await fetch('https://api.genderize.io/?name=hassan');
+    let name = document.getElementById('name').value;
+    console.log(name);
+    const response = await fetch('https://api.genderize.io/?name=' + name);
     const responseJson = await response.json(); //extract JSON from the http response
     console.log(responseJson);
 }
